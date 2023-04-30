84d6c9cc386694959b7b01991ba35f571886f095
Noriomusha <edward.i.alvarado@gmail.com>
2023-04-30 01:43:54 -0400

    Project creation
    created skeleton for shopping list app

---
+++ public/scripts/index.js
@@ -0,0 +1,7 @@
+const addButton = document.getElementById("add-button");
+const inputField = document.getElementById("input-field");
+
+addButton.addEventListener("click", function(){
+    
+    console.log(inputField.value)
+});
